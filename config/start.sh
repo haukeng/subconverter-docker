77f25080c7c4aca4b2cb3371d9a5285743c9363c
haukeng <haukeng.ghou@gmail.com>
2021-12-06 23:29:37 +0800

    fix: add message when SUB_PASSWORD is unset

--- config/start.sh
+++ config/start.sh
@@ -2,6 +2,7 @@
 if [ -n "${SUB_PASSWORD}" ]; then
     sed -i "7s/.*/api_access_token = \"${SUB_PASSWORD}\"/" pref.toml
 else
+    echo "Your need to set environment variable SUB_PASSWORD to start the container!"
     exit 1
 fi
 
